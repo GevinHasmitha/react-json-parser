5726b33f435036506a4ed2e22255d97563420087
GevinHasmitha <gevinkarunarathne@gmail.com>
2023-11-21 09:54:38 +0530

    commit 5

--- src/App.js
+++ src/App.js
@@ -44,35 +44,15 @@ function App() {
     const pattern = /field\s+'([^']+)'/g;
     const parserMatch = content.matchAll(pattern);
 
-    // console.log("=======================")
-    // for (const match of parserMatch){
-    //   console.log(match)
-    // }
-    // console.log("=======================")
-
-    var fieldValue;
-    if (parserMatch && parserMatch.length === 2) {
-      fieldValue = parserMatch[1];
-    
-      console.log(`Field Value  ${fieldValue}`);
-    } else {
-      console.log("No match found.");
+var fieldValue = [];
+    for (const match of parserMatch){
+      if (match && match.length ===2){
+        fieldValue.push(match[1]);
+        console.log(`Field Value  ${fieldValue}`);
+      }else{
+        console.log("No match found.");
+      }
     }
-
-
-//Resource type validation------------------------------------------------------
-
-/*This will search error for below string and if there is a match the
-error is with the resourceType field*/ 
-var pattern3 = /(Failed to find FHIR profile for the resource type)/;
-const parserMatch2 = content.match(pattern3);
-
-if (parserMatch2 && parserMatch2.length === 2) {
-  fieldValue = "resourceType";
-  console.log(`Field Value : ${fieldValue}`);
-} else {
-  console.log("No resource type match found.");
-}
 
 
 
@@ -83,21 +63,41 @@ if (fieldValue){
     var pattern2 = /(\w+)\[(\d+)\]\.(\w+)/;
     // Use the pattern to match and extract values
     try {
-      var match = fieldValue.match(pattern2);
-      if (match !== null) {
-        const currentValue = patient[match[1]][match[2]][match[3]];
-        console.log("currentValue = ", currentValue);
+      for (const fieldvalue of fieldValue){
+          var match = fieldvalue.match(pattern2);
 
-        const modifiedValue = "$$" + currentValue;
-        console.log("modifiedValue = ", modifiedValue);
+          if (match !== null) {
+            //Navigating to the value in the JSON object
+            const currentValue = patient[match[1]][match[2]][match[3]];
+            console.log("currentValue = ", currentValue);
 
-        // Update the value in the JSON object
-        patient[match[1]][match[2]][match[3]] = modifiedValue;
-      }
+            const modifiedValue = "$$" + currentValue;
+            console.log("modifiedValue = ", modifiedValue);
+
+            // Update the value in the JSON object
+            patient[match[1]][match[2]][match[3]] = modifiedValue;
+          }
+    }
     } catch (error) {
       console.error(error);
     }
   }
+
+
+  //Resource type validation------------------------------------------------------
+
+/*This will search error for below string and if there is a match the
+error is with the resourceType field*/ 
+var pattern3 = /(Failed to find FHIR profile for the resource type)/;
+const parserMatch2 = content.match(pattern3);
+
+if (parserMatch2 && parserMatch2.length === 2) {
+  fieldValue.push("resourceType");
+  console.log(`Field Value : ${fieldValue}`);
+} else {
+  console.log("No resource type match found.");
+}
+
 
 
 
@@ -107,11 +107,9 @@ if (fieldValue){
 
     
 
-function findLineNo(obj, searchString=fieldValue, currentLine=0) {
+function findLineNo(obj, searchString, currentLine=0) {
 
   for (var key in obj) { 
-
-    currentLine= currentLine + 1;
 
     if(key === searchString || key === ""){  //if key is empty string, then the error is with the resourceType
         let entries = Object.entries(obj);   //Similar to map
@@ -136,13 +134,16 @@ function findLineNo(obj, searchString=fieldValue, currentLine=0) {
       findLineNo for each objject in the array. But if the current key's value is just a simple object, the the "if" will run
       executind findLineNo for the current object*/
        if(!Array.isArray(obj[key])){
-        obj=currentLine = findLineNo(obj[key], searchString, currentLine);
+
+         var obj2 = findLineNo(obj[key], searchString, currentLine);
+          obj[key] = obj2;
+
       }else{    
           for( const arrayElementIndex  in obj[key]){
 
-            var obb = findLineNo(obj[key][arrayElementIndex ], searchString, currentLine);
-            obj[key][arrayElementIndex ] = obb;
-            
+             var obb = findLineNo(obj[key][arrayElementIndex ], searchString, currentLine);
+             obj[key][arrayElementIndex ] = obb;
+
           }  
       }
   
@@ -151,33 +152,45 @@ function findLineNo(obj, searchString=fieldValue, currentLine=0) {
 
   return obj;
 }
+
+
+for (const val of fieldValue){
+  console.log(val)
+}
+
 const originalJSON = JSON.stringify(patient)
 console.log("------Original object--------");
 console.log(originalJSON);
 
-const jsonAfterLoop= findLineNo(patient);
+
+var jsonAfterLoop;
+for (const field of fieldValue){
+  jsonAfterLoop= findLineNo(patient,field);
+  patient = jsonAfterLoop;
+}
 const strAfterLoop = JSON.stringify(jsonAfterLoop)
 console.log("------FInal object--------");
 console.log(strAfterLoop);
 
-let jsonStringg = JSON.stringify(jsonAfterLoop, null, 2); // 2 is the number of spaces for indentation
+var jsonStringg = JSON.stringify(jsonAfterLoop, null, 2); // 2 is the number of spaces for indentation
 
-let linesArr = jsonStringg.split("\n");
-console.log(linesArr);
+if (jsonStringg){
+    let linesArr = jsonStringg.split("\n");
+    console.log(linesArr);
 
-for (let i = 0; i < linesArr.length; i++){
-  if (linesArr[i].includes("$$")){
-    linesArr[i] = linesArr[i].replace("$$", "");
-    linesArr[i] = `<span className="line">${linesArr[i]}</span>`;
+    for (let i = 0; i < linesArr.length; i++){
+      if (linesArr[i].includes("$$")){
+        linesArr[i] = linesArr[i].replace("$$", "");
+        linesArr[i] = `<span className="line">${linesArr[i]}</span>`;
 
-  }
-}
-console.log(linesArr);
-let modifiedJsonStringg = linesArr.join("\n");
-document.getElementById('content').innerHTML = `<pre>${modifiedJsonStringg}</pre>`;
+      }
+    }
+    console.log(linesArr);
+    let modifiedJsonStringg = linesArr.join("\n");
+    document.getElementById('content').innerHTML = `<pre>${modifiedJsonStringg}</pre>`;
+    }
 
-
-  };
+};
 
   return (
     <div className="App">
